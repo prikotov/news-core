12683f96fa8e0b1417df8e41be9815dffcd11298
Dmitry Prikotov <dmitry@prikotov.ru>
2026-03-22 09:02:14 +0700

    fix: change cache path to data/news-rss/cache

--- src/Service/Cache/CacheService.php
+++ src/Service/Cache/CacheService.php
@@ -11,7 +11,7 @@ use Psr\Log\LoggerInterface;
 
 final class CacheService implements CacheServiceInterface
 {
-    private const CACHE_DIR = 'data/news-rss';
+    private const CACHE_DIR = 'data/news-rss/cache';
 
     public function __construct(
         private readonly LoggerInterface $logger,
